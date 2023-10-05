d638eb1eab6d195d3443268d3427b481affa4775
IDaniil228 <99666220+IDaniil228@users.noreply.github.com>
2023-10-05 21:56:46 +0300

    Add project files.

---
+++ DZ_4(2)/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DZ_4_2_
+{
+    enum Swear { Норм, Сред, Высок }
+    struct Grandpa
+    {
+        public string name;
+        public Swear swers;
+        public string[] phrases;
+        public int bruises;
+
+        public int Bruises(string[] phrases,ref int bruises)
+        {
+            bruises = phrases.Length;
+            return bruises;
+        }
+    }
+    internal class Program
+    {
+        static void Draw(int digit)
+        {
+            switch (digit)
+            {
+                case 0:
+                    Console.WriteLine(" #### ");
+                    Console.WriteLine("#    #\n#    #\n#    #\n#    #");
+                    Console.WriteLine(" #### ");
+                    break;
+                case 1:
+                    Console.WriteLine("#\n#\n#\n#");
+                    break;
+                case 2:
+                    Console.WriteLine(" ## \n#  #\n  # \n #  \n####");
+                    break;
+                case 3:
+                    Console.WriteLine("####\n   #\n  ##\n   #\n####");
+                    break;
+                case 4:
+                    Console.WriteLine("#  #\n#  #\n#  #\n####\n   #\n   #\n   #");
+                    break;
+                case 5:
+                    Console.WriteLine("####\n#   \n####\n   #\n####");
+                    break;
+                case 6:
+                    Console.WriteLine("####\n#   \n####\n#  #\n####");
+                    break;
+                case 7:
+                    Console.WriteLine("####\n   #\n  ##\n   #\n   #");
+                    break;
+                case 8:
+                    Console.WriteLine(" ## \n#  #\n#  #\n ## \n#  #\n#  #\n ## ");
+                    break;
+                case 9:
+                    Console.WriteLine(" ## \n#  #\n#  #\n ###\n   #\n   #\n####");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("НУЖНО ВВЕСТИ ЦИФРУ ОТ 0 ДО 9");
+                    Thread.Sleep(3000);
+                    Console.BackgroundColor= ConsoleColor.Black;
+                    break;
+            }
+
+        }
+        static int Array_1(ref int composition, out double sr, params int[] numbers)
+        {
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                composition *= numbers[i];
+            }
+            sr = numbers.Sum() / numbers.Length;
+            return numbers.Sum();
+        }
+        static bool Swap(int digit_1, int digit_2, ref int[] array) 
+        {
+            int index_1 = Array.IndexOf(array, digit_1);
+            int index_2 = Array.IndexOf(array, digit_2);
+            bool flag = true;
+            if (index_1 != -1 && index_2 != -1)
+            {
+                int temp = array[index_1];
+                array[index_1] = array[index_2];
+                array[index_2] = temp;
+                return flag;
+            }
+            else
+            {
+                Console.WriteLine("В массиве нет таких элементов!");
+                flag = false;
+                return flag;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1  Поменять два чила в массиве!");
+            int[] array = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++) 
+            {
+                array[i] = rand.Next(100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Введите первое число:");
+            bool isNum_1 = int.TryParse(Console.ReadLine(), out int digit_1);
+
+            Console.WriteLine("Введите второе число:");
+            bool isNum_2 = int.TryParse(Console.ReadLine(), out int digit_2);
+            if (isNum_1 && isNum_2)
+            {
+                if (Swap(digit_1, digit_2, ref array))
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Нужно вводить числа!");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 2  Написать метод, где в качества аргумента будет передан массив");
+            int composition = 1;
+            double sr;
+            Console.WriteLine($"Сумма массива - {Array_1(ref composition, out sr, 1, 2, 3, 4, 5)}");
+            Console.WriteLine($"Произведение массива - {composition}");
+            Console.WriteLine($"Ср.знач. массива - {sr}");
+
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 3 консоли нарисовать изображение цифры в виде символа #");
+            while (true)
+            {
+                Console.WriteLine("Введите число от 0 до 9, exit или закрыть");
+                string word = Console.ReadLine().ToLower();
+                bool isNumberr_2 = int.TryParse(word, out int digit);
+                if (isNumberr_2)
+                {
+                    Draw(digit);
+                }
+                else if (word == "exit" || word == "закрыть")
+                {
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        throw new Exception();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Нужно ввести число от 0 до 9, exit или закрыть");
+                    }
+                    
+                }
+            
+
+            }
+            Environment.Exit(0);
+
+            Console.WriteLine("Задание 4  Создать структуры Дедов");
+            Grandpa pa_1 = new Grandpa();
+            pa_1.name = "Петя";
+            pa_1.swers = Swear.Норм;
+            pa_1.bruises = 0;
+            pa_1.phrases = new string[] {"Гады"};
+
+            Grandpa pa_2 = new Grandpa();
+            pa_2.name = "Аркадий";
+            pa_2.swers = Swear.Норм;
+            pa_2.bruises = 0;
+            pa_2.phrases = new string[] { "Гады", "Проститутки"};
+
+            Grandpa pa_3 = new Grandpa();
+            pa_3.name = "Иван";
+            pa_3.swers = Swear.Сред;
+            pa_3.bruises = 0;
+            pa_3.phrases = new string[] {"Гады", "Проститутки", "Суки"};
+
+            Grandpa pa_4 = new Grandpa();
+            pa_4.name = "Святослав";
+            pa_4.swers = Swear.Сред;
+            pa_4.bruises = 0;
+            pa_4.phrases = new string[] { "Гады", "Проститутки", "Суки", "Черти"};
+
+            Grandpa pa_5 = new Grandpa();
+            pa_5.name = "Иван";
+            pa_5.swers = Swear.Высок;
+            pa_5.bruises = 0;
+            pa_5.phrases = new string[] { "Гады", "Проститутки", "Суки", "Черти", "Уроды"};
+
+            Console.WriteLine($"Дед {pa_1.name} получил {pa_1.Bruises(pa_1.phrases,ref pa_1.bruises)} фингалов от бабки");
+            Console.WriteLine($"Дед {pa_2.name} получил {pa_2.Bruises(pa_2.phrases, ref pa_2.bruises)} фингалов от бабки");
+            Console.WriteLine($"Дед {pa_3.name} получил {pa_3.Bruises(pa_3.phrases, ref pa_3.bruises)} фингалов от бабки");
+            Console.WriteLine($"Дед {pa_4.name} получил {pa_4.Bruises(pa_4.phrases, ref pa_4.bruises)} фингалов от бабки");
+            Console.WriteLine($"Дед {pa_5.name} получил {pa_5.Bruises(pa_5.phrases, ref pa_5.bruises)} фингалов от бабки");
+
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZ_4/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+namespace DZ_4
+{
+    internal class Program
+    {
+        static int Fibanachi(int number_2)
+        {
+            if (number_2 <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibanachi(number_2 - 2) + Fibanachi(number_2 - 1);
+            }
+        }
+        static int NOD(int dgt, int dgt_1)
+        {
+            int maxi = Math.Max(dgt, dgt_1);
+            int mini = Math.Min(dgt, dgt_1);
+            int result = Math.Min(dgt, dgt_1);
+            while (maxi % mini != 0)
+            {
+                result = maxi % mini;
+                int temp = mini;
+                mini = maxi % mini;
+                maxi = temp;
+
+            }
+            return result;
+        }
+        static int Factorial_1(int digit)
+        {
+
+            if (digit == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return checked(digit * Factorial_1(digit - 1));
+            }
+
+        }
+        static bool Factorial(int digit, out int factorial)
+        {
+            factorial = 1;
+            try
+            {
+                for (int i = 1; i <= digit; i++)
+                {
+                    checked
+                    {
+                        factorial *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+        }
+        static void SwapPlace(ref double var_1, ref double var_2)
+        {
+            double temp = var_1;
+            var_1 = var_2;
+            var_2 = temp;
+        }
+        static double Max(double first, double second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 5.1 Наибольшее из двух чисел.");
+            Console.Write("Введите первое число: ");
+            bool isNum = double.TryParse(Console.ReadLine(), out double first);
+            Console.Write("Введите второе число: ");
+            bool isNum_1 = double.TryParse(Console.ReadLine(), out double second);
+            if (isNum && isNum_1)
+            {
+                Console.WriteLine(Max(first, second));
+            }
+            else 
+            {
+                Console.WriteLine("Нужно ввести числа");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 5.2 Меняет местами значения двух передаваемых параметров.");
+            Console.Write("Введите первую переменную: ");
+            bool isNum_11 = double.TryParse(Console.ReadLine(), out double var_1);
+            Console.Write("Введите вторую переменную: ");
+            bool isNum_00 = double.TryParse(Console.ReadLine(), out double var_2);
+            if (isNum_11 && isNum_00)
+            {
+                SwapPlace(ref var_1,ref var_2);
+                Console.WriteLine($"Значение первой переменной - {var_1}, второй - {var_2}");
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести числа");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 5.3  Написать метод вычисления факториала числа");
+            Console.WriteLine("Введите число: ");
+            bool isNumber = int.TryParse(Console.ReadLine(), out int digit);
+            if (isNumber && digit > 0)
+            {
+                if (Factorial(digit, out int factorial))
+                {
+                    Console.WriteLine($"Факториал {digit} равен - {factorial}");
+                }
+                else
+                {
+                    Console.WriteLine("Возникто переполнение");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести число");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 5.4 Написать рекурсивный метод вычисления факториала числа");
+            Console.WriteLine("Введите число: ");
+            bool isNumber_1 = int.TryParse(Console.ReadLine(), out int digit_1);
+            if (isNumber_1 && digit_1 > 0)
+            {
+                try 
+                {
+                    Console.WriteLine($"Факториал числа равен - {Factorial_1(digit_1)}");
+                }
+                catch (OverflowException) 
+                {
+                    Console.WriteLine("Возникло переволнение!");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести число");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Домашнее задание 5.1 Написать метод, который вычисляет НОД");
+            Console.WriteLine("Введите первое число:");
+            bool num_11 = int.TryParse(Console.ReadLine(), out int dgt);
+            Console.WriteLine("Введите второе число:");
+            bool num_12 = int.TryParse(Console.ReadLine(), out int dgt_1);
+            if (num_11 && num_12)
+            {
+                Console.WriteLine($"Наибольший общий делитель - {NOD(dgt, dgt_1)}");
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести числа");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Домашнее задание 5.2 вычислить значение n-го числа ряда Фибоначчи.");
+            Console.WriteLine("Введите номер числа: ");
+            bool isNum_3 = int.TryParse(Console.ReadLine(), out int number_2);
+            if (isNum_3)
+            {
+                Console.WriteLine(Fibanachi(number_2)); ;
+            }
+            else 
+            {
+                Console.WriteLine("Нужно ввести число!");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+        }   
+    }
+}
